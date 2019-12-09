9dd9af885bf01dd7ae4729c193a1977e0945673e
Kelsey Neis <neis@umn.edu>
2019-12-09 12:00:38 -0600

    Fix #4 Make Dad Joke Guy interactible

--- adventuretutorial/world.py
+++ adventuretutorial/world.py
@@ -17,14 +17,12 @@ def load_tiles():
     """Parses a file that describes the world space into the _world object"""
     with open('../resources/map.txt', 'r') as f:
         rows = f.readlines()
-    x_max = len(rows[0].split('\t'))
     for y in range(len(rows)):
-        cols = rows[y].split('\t')
+        x_max = len(rows[y].split('|'))
+        cols = rows[y].split('|')
         for x in range(x_max):
             tile_name = cols[x].replace('\n', '')
             if tile_name == 'StartingRoom':
                 global starting_position
                 starting_position = (x, y)
             _world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
-
-
