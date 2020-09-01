1c4cf13c5d717c4c12ce765daceaeb932d5c62da
Kelsey Neis <neis@umn.edu>
2020-08-31 22:39:58 -0500

    Playing with curses

--- adventuretutorial/gameWindow.py
+++ adventuretutorial/gameWindow.py
@@ -3,28 +3,60 @@ import curses
 import time
 from curses import wrapper
 
-# sc = curses.initscr()
+
+class GameWindow():
+    def __init__(self):
+        screen = curses.initscr()
+        screen.refresh()
+        self.height, self.width = screen.getmaxyx()  # 40, 111
+        self.win = curses.newwin(self.height, self.width, 0, 0)
+        self.win.refresh()
+        curses.curs_set(2)
+        curses.cbreak()
+        self.win.keypad(True)
+        self.win.scrollok(True)
+        # # curses.echo(True)
+
+    def play(self):
+        try:
+            for i in range(50):
+                self.add_text("hello, my name is kelsey. I'm from Minnesota hello, my name is kelsey. I'm from Minnesota hello, my name is kelsey. I'm from Minnesota hello, my name is kelsey. I'm from Minnesota hello, my name is kelsey. I'm from Minnesota")
+                curses.napms(100)
+        except:
+            print("error occurred")
+        curses.napms(3000)
+        curses.endwin()
+
+    def add_text(self, text):
+        current_y, current_x = curses.getsyx()
+        if(current_y >= self.height - 1):
+            self.win.scroll(10)
+            current_y = current_y - 10
+        self.win.addstr(current_y + 1, 0, text)
+        self.win.refresh()  # y,x coord of top left corner of pad in display; min y,x coord of screen; max y, x coord of screen
+
+if __name__ == '__main__':
+    GameWindow().play()
+# mypad.chgat(i, 0, 10)
 
 
-def main(stdscr):
-    h, w = stdscr.getmaxyx()
-    win = curses.newwin(h, w, 0, 0)
+# while True:
+#     c = win.getch()
+#     y, x = curses.getsyx()
+#     if c == ord('q'):
+#         break  # Exit the while loop
+#     elif c == curses.KEY_UP and y > 0:
+#         win.chgat(y - 1, x, 10)
+#         win.refresh()
+#     elif c == curses.KEY_RIGHT and x < w-1:
+#         win.chgat(y, x + 1, 10)
+#         win.refresh()
+#     elif c == curses.KEY_DOWN and y < h-1:
+#         win.chgat(y + 1, x, 10)
+#         win.refresh()
+#     elif c == curses.KEY_LEFT and x > 0:
+#         win.chgat(y, x - 1, 10)
+#         win.refresh()
+#
+# wrapper(main)
 
-    # curses.curs_set(0)
-    curses.cbreak()
-    win.keypad(True)
-    # curses.echo(True)
-    stdscr.refresh()
-    for i in range(24):
-        win.addstr("hello, my name is kelsey"[i])
-        time.sleep(.1)
-        win.refresh()
-    while True:
-        c = stdscr.getch()
-        if c == ord('q'):
-            break  # Exit the while loop
-        elif c == ord('p'):
-            win.addch(5, 10, curses.ACS_DIAMOND)
-            win.refresh()
-
-wrapper(main)
